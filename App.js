31cecc69f24a67f8c4bd6a31a21efc8a27498720
littlekimber <zhyan0096@gmail.com>
2022-05-21 19:43:42 -0700

    First version of UI, missing recording function

--- App.js
+++ App.js
@@ -1,12 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, Button, StyleSheet, Text, SafeAreaView, TouchableOpacity} from 'react-native';
+import React from 'react';
+import AudioRecorderPlayer, { 
+  AVEncoderAudioQualityIOSType,
+  AVEncodingOption, 
+  AudioEncoderAndroidType,
+  AudioSet,
+  AudioSourceAndroidType, 
+ } from 'react-native-audio-recorder-player';
 
 export default function App() {
+  const [recording, setRecording] = React.useState();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <SafeAreaView>
+        <Text className='title' style={styles.title}>
+          Hit the button to record
+        </Text>
+      </SafeAreaView>
+      <SafeAreaView>
+        <Text>
+          00:00
+        </Text>
+      </SafeAreaView>
+      <SafeAreaView style={{
+        flexDirection: 'row'
+      }}>
+        <TouchableOpacity onPress={()=>{console.log('recording')}}>
+          <Image
+            source={require('./assets/start.png')}
+            style={styles.image}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>{console.log('stop')}}>
+          <Image 
+            source={require('./assets/stop.png')}
+            style={styles.image}
+          />
+        </TouchableOpacity>
+      </SafeAreaView>
+
       <StatusBar style="auto" />
-    </View>
+
+      <SafeAreaView style={{
+        flexDirection: 'row',
+        margin: 20
+      }}>
+        <Text style={styles.text}>start</Text>
+        <Text style={styles.text}>stop</Text>
+      </SafeAreaView>
+
+      <SafeAreaView style={{
+        flexDirection: 'row'
+      }}>
+        <TouchableOpacity onPress={()=>{console.log('replay music')}}>
+          <Image 
+            source={require('./assets/replay.png')}
+            style={styles.image}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={()=>{console.log('replay stop')}} disabled={true}>
+          <Image
+              source={require('./assets/replaystop.png')}
+              style={styles.image}
+            />
+        </TouchableOpacity>
+      </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +77,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
+    
   },
+
+  button: {
+
+  },
+
+  image: {
+    width: 50,
+    height: 50,
+    margin: 12,
+    marginBottom: 0
+  },
+
+  text: {
+    margin: 20,
+    marginTop: 0
+  },
+
+  title: {
+    margin: 30,
+    fontSize: 20
+  }
+
 });
